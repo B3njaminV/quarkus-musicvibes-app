72659fced2fb6651e7bb41fb4e488762d11ce864
ismail.taha_janan <ismail.taha_janan@etu.uca.fr>
2023-05-26 08:26:27 +0200

    :rocket:first commit

---
+++ src/src/main/java/org/musicApp/Main.java
@@ -0,0 +1,22 @@
+package org.musicApp;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+    public static void main(String... args) {
+            Quarkus.run( Application.class, args);
+        }
+        public static class Application implements QuarkusApplication {
+
+            @Override
+            public int run(String... args) {
+                System.out.println("Do startup logic here");
+                Quarkus.waitForExit();
+                return 0;
+            }
+
+    }
+}
---
+++ src/src/test/java/org/acme/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.acme.GreetingResourceTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT {
+    // Execute the same tests but in packaged mode.
+}
