fa2eee6a73d3a673684eda54039d0ea4c8231a16
etudiant <Benjamin.valleix@gmail.com>
2023-06-01 15:15:28 +0200

    :zap: Ajout requête dans repository + ajout controller/services

--- src/src/main/java/org/musicApp/repository/MusicRepository.java
+++ src/src/main/java/org/musicApp/repository/MusicRepository.java
@@ -1,50 +1,80 @@
 package org.musicApp.repository;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
-import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
-import static com.mongodb.client.model.Filters.eq;
-
-import org.bson.codecs.configuration.CodecProvider;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import jakarta.ws.rs.*;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.musicApp.module.Music;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 
 @Singleton
 public class MusicRepository {
 
-    private MusicRepository musicManager = new MusicRepository();
+    private final MongoCollection<Document> collection;
+    private final MongoDatabase bdd;
+    private static final String MONGODB_URL = "mongodb://localhost:27017";
+    private static final String MONGODB_DATABASE = "lequipe";
 
-    CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
-    CodecRegistry pojoCodecRegistry  = fromRegistries(getDefaultCodecRegistry(),fromProviders(pojoCodecProvider));
-
-
-    @ConfigProperty(name = "mongo.string")
-    String uri ;
-
-    @ConfigProperty(name = "mongo.database")
-    String dbName ;
-
-    public void getMusics(){
-        System.out.println("startGet");
-        try(MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("dbName").withCodecRegistry(pojoCodecRegistry);
-            MongoCollection<Music> collection = database.getCollection("music", Music.class);
-
-            List<Music> musics = (List<Music>) collection.find(eq("name", "Back to the Future"));
-            System.out.println(musics);
-        }
+    @Inject
+    public MusicRepository() {
+        MongoClient db = MongoClients.create(MONGODB_URL);
+        this.bdd = db.getDatabase(MONGODB_DATABASE);
+        this.collection = bdd.getCollection("music");
     }
 
+    public List<Music> getAllMusics() {
+        List<Music> musics = new ArrayList<>();
+        for (Document doc : collection.find()) {
+            Music music = documentToMusic(doc);
+            musics.add(music);
+        }
+        return musics;
+    }
+
+    public Music getMusicById(@PathParam("id") String id) {
+        Document doc = collection.find(new Document("_id", new ObjectId(id))).first();
+        if (doc != null) {
+            return documentToMusic(doc);
+        }
+        return null;
+    }
+
+    public void addMusic(Music music) {
+        Document doc = musicToDocument(music);
+        collection.insertOne(doc);
+        music.setId(doc.getObjectId("_id").toString());
+    }
+
+    public void updateMusic(@PathParam("id") String id, Music oldmusic) {
+        Document filter = new Document("_id", new ObjectId(id));
+        Document update = musicToDocument(oldmusic);
+        collection.updateOne(filter, new Document("$set", update));
+    }
+
+    public void deleteMusic(@PathParam("id") String id) {
+        collection.deleteOne(new Document("_id", new ObjectId(id)));
+    }
+
+    private Document musicToDocument(Music music) {
+        return new Document("_id", new ObjectId(music.getId()))
+                .append("name", music.getName());
+    }
+
+    private Music documentToMusic(Document doc) {
+        Music music = new Music();
+        music.setId(doc.getObjectId("_id").toString());
+        music.setName(doc.getString("name"));
+        return music;
+    }
 
 }
--- src/src/main/java/org/musicApp/service/MusicService.java
+++ src/src/main/java/org/musicApp/service/MusicService.java
@@ -1,4 +1,38 @@
 package org.musicApp.service;
 
+import org.musicApp.module.Music;
+import org.musicApp.repository.MusicRepository;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+import java.util.List;
+
+@Singleton
 public class MusicService {
+    private final MusicRepository repository;
+
+    @Inject
+    public MusicService(MusicRepository musicRepository) {
+        this.repository = musicRepository;
+    }
+
+    public List<Music> getAllMusics() {
+        return repository.getAllMusics();
+    }
+
+    public Music getMusicById(String id) {
+        return repository.getMusicById(id);
+    }
+
+    public void addMusic(Music music) {
+        repository.addMusic(music);
+    }
+
+    public void updateMusic(String id, Music oldMusic) {
+        repository.updateMusic(id, oldMusic);
+    }
+
+    public void deleteMusic(String id) {
+        repository.deleteMusic(id);
+    }
 }
